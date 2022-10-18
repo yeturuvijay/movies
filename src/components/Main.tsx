8592ed502e9e7387ba48aba000e9ae8ddedee073
Rama <ramau07@gmail.com>
2022-10-17 21:42:27 -0400

    updating service details

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,26 @@
-import './Main.css'
+import { useEffect } from "react";
+import {
+  getFilteredMovies,
+  getMovieDetails,
+  getTrendingMovies,
+} from "../services/MovieService";
+import "./Main.css";
 
 const Main = () => {
-  return <div className='Main'>Main works</div>
-}
+  useEffect(() => {
+    getTrendingMovies().then((response) => {
+      console.log("test data trending movies :: ", response.results);
+    });
 
-export default Main
+    getFilteredMovies("", 30).then((response) => {
+      console.log("test data filter movies :: ", response.results);
+    });
+
+    getMovieDetails(616820).then((response) => {
+      console.log("test data movie details :: ", response);
+    });
+  }, []);
+  return <div className="Main">Main works</div>;
+};
+
+export default Main;
--- src/services/MovieService.ts
+++ src/services/MovieService.ts
@@ -1,1 +1,70 @@
-export const getTrendingMovies = () => {};
+import axios from "axios";
+import MovieDetails from "../models/MovieDetails";
+import MultipleMovieResponse from "../models/MultipleMovieResponse";
+
+interface FilterParams {
+  api_key: string;
+  language: string;
+  vote_average?: {
+    gte: number;
+  };
+  with_runtime?: {
+    lte: number;
+  };
+  with_genres?: string;
+}
+
+const api_key: string = process.env.REACT_APP_MOVIE_KEY || "";
+
+export const getTrendingMovies = (): Promise<MultipleMovieResponse> => {
+  return axios
+    .get("https://api.themoviedb.org/3/trending/all/day", {
+      params: {
+        api_key,
+      },
+    })
+    .then((response) => response.data);
+};
+
+export const getFilteredMovies = (
+  genres?: string,
+  runtime?: number,
+  rating?: number
+): Promise<MultipleMovieResponse> => {
+  let filterParams: FilterParams = {
+    api_key,
+    language: "en-US",
+  };
+
+  if (genres) {
+    filterParams.with_genres = genres;
+  }
+
+  if (runtime) {
+    filterParams.with_runtime = {
+      lte: runtime,
+    };
+  }
+
+  if (rating) {
+    filterParams.vote_average = {
+      gte: rating,
+    };
+  }
+
+  return axios
+    .get("https://api.themoviedb.org/3/discover/movie", {
+      params: filterParams,
+    })
+    .then((response) => response.data);
+};
+
+export const getMovieDetails = (id: number): Promise<MovieDetails> => {
+  return axios
+    .get(`https://api.themoviedb.org/3/movie/${encodeURIComponent(id)}`, {
+      params: {
+        api_key,
+      },
+    })
+    .then((response) => response.data);
+};
