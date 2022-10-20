003284aaf747e1533ac7bcba15c933b24974cf8d
imacrm10 <imacrm10@gmail.com>
2022-10-20 10:46:11 -0400

    minor fixes to style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,15 @@
-import { Link } from "react-router-dom";
-import "./Header.css";
+import { Link } from 'react-router-dom'
+import './Header.css'
 
 const Header = () => {
   return (
-    <header className="Header">
-      <Link to="/">
-        <h1>Movies</h1>
+    <header className='Header'>
+      <Link to='/'>
+        <h1>Movies & Shows</h1>
       </Link>
-      <Link to="movies/watchlist">Watchlist</Link>
+      <Link to='movies/watchlist'>Watchlist</Link>
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
